0b5c5521d840df73627452b6b326de7e33681f8a
Daniel Orchel <karma0rg4u@gmail.com>
2021-08-18 22:26:40 +0100

    Add components

---
+++ src/components/Card.jsx
@@ -0,0 +1,13 @@
+import React, { Children } from 'react';
+import './Card.css'
+
+const Card = (props) => {
+  const classes = 'card ' + props.className;
+  return (
+    <div className={classes}>
+      {props.children}
+    </div>
+  )
+}
+
+export default Card;
---
+++ src/components/Expenses.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ExpenseItem from './ExpenseItem';
+import './Expenses.css'
+import Card from './Card';
+
+const Expenses = (props) => {
+  const expenses = props.items;
+
+  return (
+    <Card className="expenses">
+      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}/>
+      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}/>
+      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}/>
+    </Card>
+  )
+}
+
+export default Expenses;
